067bd82b273925844e47bac7c5cc2fbf191d91ef
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2020-03-02 20:47:45 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,22 +2,4 @@
 
 FAIL=0
 
-echo "starting"
-
-npm run watch --if-present &
-sleep 5 && touch test.scss &
-
-for job in `jobs -p`
-do
-echo $job
-    wait $job || let "FAIL+=1"
-done
-
-echo $FAIL
-
-if [ "$FAIL" == "0" ];
-then
-echo "YAY!"
-else
-echo "FAIL! ($FAIL)"
-fi
+npm run watch --if-present & DEPS_PID=$! && sleep 10 && touch test.scss & kill -9 $DEPS_PID
