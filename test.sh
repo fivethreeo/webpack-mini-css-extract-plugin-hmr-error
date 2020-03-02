4493dfe1d3b63a433b20e66f502c84ab71715f85
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2020-03-02 21:24:39 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,6 +5,6 @@ FAIL=0
 npm run watch --if-present & \
   DEPS_PID=$! && \
   sleep 10 && \
-  echo Touching test.scss && touch test.scss && \
+  printf "\n\n-------------\nTouching test.scss\n-------------\n\n" && touch test.scss && \
   sleep 10 && \
-  kill -9 $DEPS_PID
+  kill -s TERM $DEPS_PID
